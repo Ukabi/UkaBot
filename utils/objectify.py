67703ab48a9f39e309f408a6c9c1bc07fad8d7a6
Ukabi <ukabi.sama@gmail.com>
2021-05-01 03:11:43 +0200

    Code Cleaning

--- utils/objectify.py
+++ utils/objectify.py
@@ -7,6 +7,7 @@ from typing import (
     Tuple,
     Union
 )
+
 from .utils import (
     isofclass,
     isoftype
--- utils/utils.py
+++ utils/utils.py
@@ -44,32 +44,6 @@ class ImprovedList(list):
 
 ############################################ FUNCTIONS ############################################
 
-def _str_key_json(value: Union[List[Any], Dict[Any, Any]]) -> Union[List[Any], Dict[str, Any]]:
-    """Recursively casts all keys in the given json-like Union[List, Dict] to `str`.
-
-    Parameters
-        value: Union[List[Any], Dict[Any, Any]]
-            The json-like object to cast keys to `str`.
-
-    Returns
-        Union[List[Any], Dict[`str`, Any]]
-            The json-like object with keys (and nested keys) casted to `str`.
-
-    """
-    if isinstance(value, dict):
-        ret = {}
-        for key, val in value.items():
-            if isinstance(val, dict):
-                val = _str_key_json(val)
-            elif isinstance(val, (list, tuple)):
-                val = [_str_key_json(v) for v in val]
-            ret[str(key)] = val
-        return ret
-    elif isinstance(value, (list, tuple)):
-        return [_str_key_json(val) for val in value]
-    else:
-        raise TypeError
-
 def flatten(l: Union[list, tuple]) -> list:
     """Flattening generic method for various usages.
 
