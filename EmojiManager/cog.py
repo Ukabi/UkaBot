fc8a990e033f11cce7bc69b70753512ba7279b14
Ukabi <ukabi.sama@gmail.com>
2021-05-05 02:01:05 +0200

    EmojiManager Update Isolated more data-side variables. Transfered some methods to static methods for clarity. And some more code cleaning. Might need to handle file as argument in `emoji add` in the future.

--- EmojiManager/cog.py
+++ EmojiManager/cog.py
@@ -1,5 +1,4 @@
 ############################################# IMPORTS #############################################
-
 #################### DISCORD ####################
 from discord import (
     Embed,
@@ -18,7 +17,11 @@ from discord.ext.commands import (
 from discord.ext.commands import group
 
 ##################### DATA ######################
-from .data import PATH
+from .data import (
+    PATH,
+    EmojiTypes,
+    RoleTypes
+)
 
 ##################### UTILS #####################
 from typing import Union
@@ -49,16 +52,9 @@ class EmojiManager(Cog):
 
     @admin_or_permissions(manage_emojis=True)
     @emoji.command()
-    async def add(
-        self, ctx: Context, file_path: str, name: str,
-        *roles: Union[int, str, Role]
-    ):
-        """
-        **[file] [name] (roles)** : adds an emoji to the server,
-        if roles, restricts it for precised roles.
-        """
+    async def add(self, ctx: Context, file_path: str, name: str, *roles: RoleTypes):
         try:
-            roles = [await RoleConverter().convert(ctx, role) for role in roles]
+            roles = [await RoleConverter().convert(ctx, str(role)) for role in roles]
         except BadArgument:
             error = InvalidArguments(
                 ctx=ctx,
@@ -66,70 +62,69 @@ class EmojiManager(Cog):
                 message="Provided roles could not be found"
             )
             await error.execute()
-            return
-
-        if file_path.startswith("http"):
-            req = rq.get(file_path)
-            if req.ok:
-                byte_data = bytearray(req.content)
-                file = io.BytesIO(bytes(req.content))
-            else:
-                error = InvalidArguments(
-                    ctx=ctx,
-                    title='Loading Error',
-                    message="Couldn't retrieve picture from privided link"
-                )
-                await error.execute()
-                return
 
         else:
-            try:
-                with open(f'{PATH}/{file_path}', 'rb') as image:
-                    raw_data = image.read()
+            if file_path.startswith("http"):
+                req = rq.get(file_path)
+                if req.ok:
+                    byte_data = bytearray(req.content)
+                    file = io.BytesIO(bytes(req.content))
+                else:
+                    error = InvalidArguments(
+                        ctx=ctx,
+                        title='Loading Error',
+                        message="Couldn't retrieve picture from privided link"
+                    )
+                    await error.execute()
+
+            else:
+                try:
+                    with open(f'{PATH}/{file_path}', 'rb') as image:
+                        raw_data = image.read()
+                except FileNotFoundError:
+                    error = InvalidArguments(
+                        ctx=ctx,
+                        title='Loading Error',
+                        message="Couldn't retrieve picture from privided path"
+                    )
+                    await error.execute()
+
+                else:
                     byte_data = bytearray((raw_data))
                     file = io.BytesIO(bytes(byte_data))
-            except FileNotFoundError:
-                error = InvalidArguments(
-                    ctx=ctx,
-                    title='Loading Error',
-                    message="Couldn't retrieve picture from privided path"
+
+            answer = await ask_confirmation(
+                ctx=ctx,
+                bot=self.bot,
+                file=File(file, filename="emoji_template.png"),
+                message=(
+                    "Waiting for confirmation\n"
+                    "Reply with y/n"
                 )
-                await error.execute()
-                return
+            )
+            if answer:
+                emoji = await ctx.guild.create_custom_emoji(
+                    name=name,
+                    image=byte_data,
+                    roles=roles
+                )
+                affected_roles = ", ".join([str(role) for role in roles]) if roles else "everyone"
+                embed = Embed(
+                    title=f"Successfully created {emoji} emoji",
+                    description=f"Affected roles: {affected_roles}"
+                )
+            else:
+                embed = Embed(
+                    title="Cancelled"
+                )
 
-        answer = await ask_confirmation(
-            ctx=ctx,
-            bot=self.bot,
-            file=File(file, filename="emoji_template.png"),
-            message=(
-                "Waiting for confirmation\n"
-                "Reply with y/n"
-            )
-        )
-        if answer:
-            emoji = await ctx.guild.create_custom_emoji(
-                name=name,
-                image=byte_data,
-                roles=roles
-            )
-            affected_roles = ", ".join([str(role) for role in roles]) if roles else "everyone"
-            embed = Embed(
-                title=f"Successfully created {emoji} emoji.",
-                description=f"Affected roles: {affected_roles}."
-            )
-        else:
-            embed = Embed(
-                title="Cancelled"
-            )
-
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
     @admin_or_permissions(manage_emojis=True)
     @emoji.command()
-    async def remove(self, ctx: Context, emoji: Union[str, int, Emoji]):
+    async def remove(self, ctx: Context, emoji: EmojiTypes):
         try:
             emoji = await EmojiConverter().convert(ctx, emoji)
-            emoji_parse = str(emoji) if emoji.available else emoji.name
         except BadArgument:
             error = InvalidArguments(
                 ctx=ctx,
@@ -137,48 +132,53 @@ class EmojiManager(Cog):
                 message="Provided emoji could not be found"
             )
             await error.execute()
-            return
 
-        answer = await ask_confirmation(
-            ctx=ctx,
-            bot=self.bot,
-            message=(
-                f"Affected emoji: {emoji_parse}\n"
-                "Answer with y/n to confirm removal."
-            )
-        )
-        if answer:
-            await emoji.delete()
-            embed = Embed(
-                title="Emoji Removed",
-                description=f"Successfully removed {emoji_parse} from server"
-            )
         else:
-            embed = Embed(
-                title="Cancelled"
+            emoji_parse = str(emoji) if emoji.available else emoji.name
+            answer = await ask_confirmation(
+                ctx=ctx,
+                bot=self.bot,
+                message=(
+                    f"Affected emoji: {emoji_parse}\n"
+                    "Answer with y/n to confirm removal"
+                )
             )
+            if answer:
+                await emoji.delete()
+                embed = Embed(
+                    title="Emoji Removed",
+                    description=f"Successfully removed {emoji_parse} from server"
+                )
+            else:
+                embed = Embed(
+                    title="Cancelled"
+                )
 
-        await ctx.send(embed= embed)
+            await ctx.send(embed= embed)
 
     @admin_or_permissions(manage_emojis=True)
     @emoji.command(name='list')
     async def list_(self, ctx: Context):
         emojis = sorted(ctx.guild.emojis, key=lambda e: e.name)
 
-        if not emojis:
-            message = "No emoji"
+        if emojis:
+            message = "\n".join(self.emoji_infos(emoji) for emoji in emojis)
         else:
-            message = ""
-            for emoji in emojis:
-                message += str(emoji) if emoji.available else emoji.name
-                if emoji.managed:
-                    message += " - Twitch"
-                elif emoji.roles:
-                    message += f' - Roles: {", ".join([str(role) for role in emoji.roles])}'
-                message += "\n"
+            message = "No emoji"
 
         embed = Embed(
             title="Server emojis list",
             description=message
         )
         await ctx.send(embed=embed)
+
+    ######################################## STATIC METHODS #######################################
+
+    @staticmethod
+    def emoji_infos(emoji: Emoji) -> str:
+        r = str(emoji) if emoji.available else emoji.name
+        if emoji.managed:
+            r += " - Twitch"
+        elif emoji.roles:
+            r += f' - Roles: {", ".join([str(role) for role in emoji.roles])}'
+        return emoji
--- EmojiManager/data.py
+++ EmojiManager/data.py
@@ -1,7 +1,14 @@
 ############################################# IMPORTS #############################################
 
+from discord import (
+    Emoji,
+    Role
+)
 from pathlib import Path
+from typing import Union
 
 ############################################# GLOBALS #############################################
 
 PATH = f'{Path(__file__).absolute()}/emojis'
+EmojiTypes = Union[int, str, Emoji]
+RoleTypes = Union[int, str, Role]
